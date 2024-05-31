49c4ebccfaa6c12d2f952ecbbe2e8ee5c1fa3974
DuoMinhHieu <dmh6774@gmail.com>
2024-05-31 08:39:15 +0700

    main

---
+++ springbootcustomermanagement/src/main/java/com/codegym/SpringbootcustomermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcustomermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcustomermanagementApplication.class, args);
+	}
+
+}
